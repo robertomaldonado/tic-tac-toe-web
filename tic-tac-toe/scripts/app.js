2b5781753917e3ca883bbc7597910a78931f92b7
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-05-15 13:24:22 -0500

    Prevent defaul in form submit

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,12 +1,17 @@
+// Elements related to the overlay
 const playerConfigOverlayElm = document.getElementById("config-overlay");
 const backdropElm = document.getElementById("backdrop");
-
+// Select the form element
+const formElm = document.querySelector("form");
+// Elements related to the game config
 const editPlaerOneBtnElm = document.getElementById("edit-player-one-btn");
 const editPlaerTwoBtnElm = document.getElementById("edit-player-two-btn");
 const cancelConfigBtnElm = document.getElementById("cancel-config-btn");
-
+// Add event listeners to the player buttons
 editPlaerOneBtnElm.addEventListener("click", openPlayerConfig);
 editPlaerTwoBtnElm.addEventListener("click", openPlayerConfig);
-
+// Add event listeners to the player overlay
 cancelConfigBtnElm.addEventListener("click", closePlayerConfig);
 backdropElm.addEventListener("click", closePlayerConfig);
+// Add event listener to the form submit button
+formElm.addEventListener("submit", savePlayerConfig);
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -7,3 +7,11 @@ function closePlayerConfig() {
   playerConfigOverlayElm.style.display = "none";
   backdropElm.style.display = "none";
 }
+
+const savePlayerConfig = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target); // Get the form data
+  const userPlayerName = formData.get("player-name"); // Get the value of the input field
+  userPlayerName = userPlayerName.trim(); // Remove any leading or trailing spaces
+  closePlayerConfig(); // Close the overlay
+};
