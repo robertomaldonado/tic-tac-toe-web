b73bac5c9cb9e6dbb718a13a331a27920a4dc15d
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-05-15 14:03:26 -0500

    add error class. and reset to cancel

--- tic-tac-toe/scripts/app.js
+++ tic-tac-toe/scripts/app.js
@@ -1,17 +1,26 @@
+let editedPlayer = null;
 // Elements related to the overlay
 const playerConfigOverlayElm = document.getElementById("config-overlay");
 const backdropElm = document.getElementById("backdrop");
+
 // Select the form element
 const formElm = document.querySelector("form");
+
+// Select the element to display errors
+const errorsOutputElm = document.getElementById("config-errors");
+
 // Elements related to the game config
 const editPlaerOneBtnElm = document.getElementById("edit-player-one-btn");
 const editPlaerTwoBtnElm = document.getElementById("edit-player-two-btn");
 const cancelConfigBtnElm = document.getElementById("cancel-config-btn");
+
 // Add event listeners to the player buttons
 editPlaerOneBtnElm.addEventListener("click", openPlayerConfig);
 editPlaerTwoBtnElm.addEventListener("click", openPlayerConfig);
+
 // Add event listeners to the player overlay
 cancelConfigBtnElm.addEventListener("click", closePlayerConfig);
 backdropElm.addEventListener("click", closePlayerConfig);
+
 // Add event listener to the form submit button
 formElm.addEventListener("submit", savePlayerConfig);
--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -1,17 +1,27 @@
-function openPlayerConfig() {
+function openPlayerConfig(evt) {
+  editedPlayer = +evt.target.dataset.playerid; // Get the player ID, covert to int
   playerConfigOverlayElm.style.display = "block";
   backdropElm.style.display = "block";
 }
 
 function closePlayerConfig() {
-  playerConfigOverlayElm.style.display = "none";
-  backdropElm.style.display = "none";
+  playerConfigOverlayElm.style.display = "none"; // Hide the overlay
+  backdropElm.style.display = "none"; // Hide the backdrop
+  formElm.firstElementChild.classList.remove("error"); // Remove the error class from the input
+  errorsOutputElm.textContent = ""; // Clear the error message
+  formElm.reset(); // Reset the form
 }
 
-const savePlayerConfig = (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target); // Get the form data
-  const userPlayerName = formData.get("player-name"); // Get the value of the input field
-  userPlayerName = userPlayerName.trim(); // Remove any leading or trailing spaces
+const savePlayerConfig = (evt) => {
+  evt.prevtDefault();
+  const formData = new FormData(evt.target); // Get the form data
+  const userPlayerName = formData.get("player-name").trim(); // Remove any leading or trailing spaces
+
+  if (!userPlayerName) {
+    evt.target.firstElementChild.classList.add("error"); // Add a class to the input element
+    errorsOutputElm.textContent = "Please enter a valid name";
+    return; //Stop execution if the name is invalid
+  }
+
   closePlayerConfig(); // Close the overlay
 };
