d4543a3a81e871dac471953f3f93bcda06c0c573
Roberto Maldonado <rbt.maldonado@gmail.com>
2024-05-15 17:31:33 -0500

    Change playername on change

--- tic-tac-toe/scripts/config.js
+++ tic-tac-toe/scripts/config.js
@@ -1,7 +1,9 @@
-function openPlayerConfig(evt) {
-  editedPlayer = +evt.target.dataset.playerid; // Get the player ID, covert to int
+function openPlayerConfig(event) {
+  const selectedPlayerId = event.target.dataset.playerid; // Get the player ID
+  editedPlayer = selectedPlayerId; // Set the edited player
   playerConfigOverlayElm.style.display = "block";
   backdropElm.style.display = "block";
+  console.log(editedPlayer);
 }
 
 function closePlayerConfig() {
@@ -12,16 +14,22 @@ function closePlayerConfig() {
   formElm.reset(); // Reset the form
 }
 
-const savePlayerConfig = (evt) => {
-  evt.prevtDefault();
-  const formData = new FormData(evt.target); // Get the form data
+const savePlayerConfig = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target); // Get the form data
   const userPlayerName = formData.get("player-name").trim(); // Remove any leading or trailing spaces
 
   if (!userPlayerName) {
-    evt.target.firstElementChild.classList.add("error"); // Add a class to the input element
+    event.target.firstElementChild.classList.add("error"); // Add a class to the input element
     errorsOutputElm.textContent = "Please enter a valid name";
     return; //Stop execution if the name is invalid
   }
+
+  const updatedPlayerDataElm = document.getElementById(
+    `player-${editedPlayer}-data`
+  ); // Get the player name element
+
+  updatedPlayerDataElm.children[1].textContent = userPlayerName; // Update the player name
 
   closePlayerConfig(); // Close the overlay
 };
